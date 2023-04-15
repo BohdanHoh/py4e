e9c19f766aa509563161b8244025c24e374da5f0
bohdanh <bohdan.stephen@gmail.com>
2023-04-15 14:04:11 -0400

    new python for everybody exercises

---
+++ ex_02_03/ex_02_03.py
@@ -0,0 +1,4 @@
+hours = input("How many hours per week: ")
+rate = input("What is your rate per hour: ")
+compensation = float(hours) * float(rate)
+print("Your weekly compensation is", compensation)
